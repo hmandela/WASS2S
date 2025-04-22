1159021543d1398204417c3060f5a0e550d477ec
hmandela <hmandelahmadiba@yahoo.fr>
2025-04-23 00:16:05 +0100

    m

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="wass2s",
-    version="0.1.5",
+    version="0.1.6",
     author="HOUNGNIBO C. M. Mandela",
     author_email="hmandelahmadiba@gmail.com",
     description="A Python package for seasonal climate forecast.",
