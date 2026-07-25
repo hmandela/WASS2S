92155034aff223e29d5c0405f94f04c196f82874
hmandela <hmandelahmadiba@yahoo.fr>
2026-07-25 23:21:10 +0100

    remove file1.txt

--- wass2s/.ipynb_checkpoints/some_correctifs_for_Linearmodels_and_PCR-checkpoint.py
+++
@@ -1,1 +0,0 @@
-### 1 - Je dois revoir et tenir compte du leakage dans le module linear model mais aussi dans PCR très important(cela concerne la façon de standardisation et de dertrend dans les deux cas
